c0efc10f21fd7461e32fb173578d29c1c6975979
Aya <ayaa.aabid@gmail.com>
2025-01-09 21:01:42 +0100

    Add Story

--- frontend/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/travel-story-app/src/pages/Home/AddEditTravelStory.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { MdAdd, MdClose, MdUpdate, MdDeleteOutline } from "react-icons/md";
 import { DateSelector } from "../../components/Input/DateSelector";
+import { ImageSelector } from "../../components/Input/ImageSelector";
+import { TagInput } from "../../components/Input/TagInput";
+import axiosInstance from "../../utils/axiosInstance";
+import moment from "moment";
+import { toast } from "react-toastify";
+import uploadImage from "../../utils/uploadImage";
 
 export const AddEditTravelStory = ({
   storyInfo,
@@ -13,7 +19,67 @@ export const AddEditTravelStory = ({
   const [story, setStory] = useState("");
   const [visitedLocation, setVisitedLocation] = useState([]);
   const [visitedDate, setVisitedDate] = useState(null);
-  const handleAddOrUpdateClick = () => {};
+  const [error, setError] = useState("");
+
+  // Add New Travel Story
+  const addNewTravelStory = async () => {
+    try {
+      let imageUrl = "";
+
+      // Upload image if present
+      if (storyImg) {
+        const imgUploadRes = await uploadImage(storyImg);
+        // Get Image URL
+        imageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post("/add-tavel-story", {
+        title,
+        story,
+        imageUrl: imageUrl || "",
+        visitedLocation,
+        visitedDate: visitedDate
+          ? moment(visitedDate).valueOf()
+          : moment().valueOf(),
+      });
+
+      if (response.data && response.data.story) {
+        toast.success("Story Added Successfully");
+        getAllTravelStories();
+        onClose();
+      }
+    } catch (error) {
+      console.error("Error adding travel story:", error);
+    }
+  };
+
+  // Update Travel Story
+  const updateTravelStory = async () => {};
+
+  const handleAddOrUpdateClick = () => {
+    console.log("Input Data:", {title, storyImg, story, visitedLocation, visitedDate,});
+
+    if (!title) {
+      setError("Please enter the title ! ");
+      return;
+    }
+
+    if (!story) {
+      setError("Please enter the descprition ! ");
+      return;
+    }
+
+    setError("");
+
+    if (type === "edit") {
+      updateTravelStory();
+    } else {
+      addNewTravelStory ();
+    }
+  };
+
+  // Delete story image and update story
+  const handleDeleteStoryImg = async () => {};
   return (
     <div>
       <div className="flex items-center justify-between">
@@ -24,7 +90,7 @@ export const AddEditTravelStory = ({
         <div>
           <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
             {type === "add" ? (
-              <button className="btn-small" onClick={() => {}}>
+              <button className="btn-small" onClick={handleAddOrUpdateClick}>
                 <MdAdd className="text-lg" /> ADD STORY
               </button>
             ) : (
@@ -43,6 +109,10 @@ export const AddEditTravelStory = ({
               <MdClose className="text-xl text-slate-400" />
             </button>
           </div>
+
+          {error && (
+            <p className="text-red-500 text-xs pt-2 text-right">{error}</p>
+          )}
         </div>
       </div>
 
@@ -60,6 +130,12 @@ export const AddEditTravelStory = ({
             <DateSelector date={visitedDate} setDate={setVisitedDate} />
           </div>
 
+          <ImageSelector
+            image={storyImg}
+            setImage={setStoryImg}
+            handleDeleteImg={handleDeleteStoryImg}
+          />
+
           <div className="flex flex-col gap-2 mt-4">
             <label className="input-label">STORY</label>
             <textarea
@@ -70,6 +146,11 @@ export const AddEditTravelStory = ({
               value={story}
               onChange={({ target }) => setStory(target.value)}
             />
+          </div>
+
+          <div className="pt-3">
+            <label className="input-label">VISITED LOCATIONS</label>
+            <TagInput tags={visitedLocation} setTags={setVisitedLocation} />
           </div>
         </div>
       </div>
